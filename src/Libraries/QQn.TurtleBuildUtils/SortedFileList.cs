f89d38326636f47cf0aac7549018582c00e48b87
Bert Huijben <rhuijben@users.noreply.github.com>
2007-08-24 16:02:23 +0000

    Fixed build

---
+++ src/Libraries/QQn.TurtleBuildUtils/SortedFileList.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace QQn.TurtleBuildUtils
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class SortedFileList : SortedList<string, string>, IEnumerable<string>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SortedFileList"/> class.
+		/// </summary>
+		public SortedFileList()
+			: base(StringComparer.InvariantCultureIgnoreCase)
+		{
+		}
+
+		/// <summary>
+		/// Adds the specified filename.
+		/// </summary>
+		/// <param name="filename">The filename.</param>
+		public void Add(string filename)
+		{
+			Add(filename, filename);
+		}
+
+		/// <summary>
+		/// Returns an enumerator that iterates through the collection.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="T:System.Collections.Generic.IEnumerator&lt;string&gt;"/> that can be used to iterate through the collection.
+		/// </returns>
+		public new IEnumerator<string> GetEnumerator()
+		{
+			return Keys.GetEnumerator();
+		}
+
+		/// <summary>
+		/// Determines whether [contains] [the specified filename].
+		/// </summary>
+		/// <param name="filename">The filename.</param>
+		/// <returns>
+		/// 	<c>true</c> if the list contains the specified filename; otherwise, <c>false</c>.
+		/// </returns>
+		public bool Contains(string filename)
+		{
+			return ContainsKey(filename);
+		}
+	}
+}
