60db13d814c710753df4788d4f136a6e1ac6edbd
Bert Huijben <rhuijben@users.noreply.github.com>
2007-11-22 18:49:36 +0000

    Fixed build

--- src/Products/QQn.TurtleMSBuild/ExternalProjects/VCBuildProject.cs
+++ src/Products/QQn.TurtleMSBuild/ExternalProjects/VCBuildProject.cs
@@ -25,7 +25,7 @@ namespace QQn.TurtleMSBuild.ExternalProjects
 			{
 				if (_resolverType == null)
 				{
-					//Type resolverType = Type.GetType("Microsoft.Build.Tasks.ResolveVCProjectOutput, Microsoft.Build.Tasks.v3.5, Version=3.5.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", false, false);
+					Type resolverType = Type.GetType("Microsoft.Build.Tasks.ResolveVCProjectOutput, Microsoft.Build.Tasks.v3.5, Version=3.5.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", false, false);
 					//if (resolverType == null || !typeof(ITask).IsAssignableFrom(resolverType))
 					resolverType = Type.GetType("Microsoft.Build.Tasks.ResolveVCProjectOutput, Microsoft.Build.Tasks, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", false, false);
 
