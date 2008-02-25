58d591702c252b31b5dcd3252e93298666b0841a
bhuijben <bhuijben@f699e4c5-0734-0410-b212-ef1cdd1c6ea5>
2008-02-25 14:50:46 +0000

    Fixed some logging message texts and added a ApplyXslTransform task to enable the ctc compilation framework
    * \src\Products\QQn.TurtleTasks\QQn.TurtleTasks.csproj
    * \src\Products\QQn.TurtleTasks\ApplyXslTransform.cs
    * \src\Products\QQn.TurtleTasks\ApplyXslTransform.XslFile.cs
    * \src\Products\QQn.TurtleTasks\ApplyXslTransform.XslFilename.cs
    * \src\Products\QQn.TurtleTasks\CachedDownloadAndExtract.cs
    * \src\Products\QQn.TurtleTasks\QQnTaskBase.cs

---
+++ trunk/src/Products/QQn.TurtleTasks/ApplyXslTransform.XslFile.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Xsl;
+using System.IO;
+using System.Xml;
+
+namespace QQn.TurtleTasks
+{
+	partial class ApplyXslTransform
+	{
+		class XslFile : XslFilename
+		{
+			DateTime _lastModified;
+
+			XslCompiledTransform _transform;
+
+			public XslFile(XslFilename filename)
+				: base(filename)
+			{
+				_lastModified = DateTime.MinValue;
+			}
+
+			public void EnsureCompiled()
+			{
+				FileInfo fif = new FileInfo(Filename);
+
+				if (!fif.Exists)
+					throw new FileNotFoundException("Xsl not found", fif.FullName);
+
+				if ((_transform != null) && fif.LastWriteTime == fif.LastWriteTime)
+					return;
+
+
+				XslCompiledTransform xc = new XslCompiledTransform(Debug);
+				XmlUrlResolver resolver = new XmlUrlResolver();
+				XsltSettings settings = new XsltSettings();
+
+
+				settings.EnableDocumentFunction = true;
+				settings.EnableScript = true;
+
+				xc.Load(fif.FullName, settings, resolver);
+
+				_lastModified = fif.LastWriteTime;
+				_transform = xc;
+			}
+
+			public bool Equals(XslFile other)
+			{
+				return base.Equals(other);
+			}
+
+			public override bool Equals(object obj)
+			{
+				return Equals(obj as XslFile);
+			}
+
+			public override int GetHashCode()
+			{
+				return Filename.GetHashCode();
+			}
+
+			public XslCompiledTransform Transform
+			{
+				get 
+				{
+					if (_transform == null)
+						throw new InvalidOperationException("Please call EnsureCompiled first");
+					
+					return _transform; 
+				}
+			}
+		}
+	}
+}
---
+++ trunk/src/Products/QQn.TurtleTasks/ApplyXslTransform.XslFilename.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QQn.TurtleTasks
+{
+	partial class ApplyXslTransform
+	{
+		class XslFilename : IEquatable<XslFilename>
+		{
+			string _filename;
+			bool _debug;
+
+			public XslFilename(string filename)
+				: this(filename, false)
+			{
+			}
+
+			public XslFilename(XslFilename filename)
+				: this(SafeFilename(filename), filename.Debug)
+			{
+			}
+
+			private static string SafeFilename(XslFilename filename)
+			{
+				if (filename == null)
+					throw new ArgumentNullException("filename");
+
+				return filename.Filename;
+			}
+
+			public XslFilename(string filename, bool debug)
+			{
+				if (string.IsNullOrEmpty(filename))
+					throw new ArgumentNullException("filename");
+
+				_filename = filename;
+				_debug = debug;
+			}
+
+			public string Filename
+			{
+				get { return _filename; }
+				set { _filename = value; }
+			}
+
+			public bool Debug
+			{
+				get { return _debug; }
+				set { _debug = value; }
+			}
+
+			#region IEquatable<XslFilename> Members
+
+			public bool Equals(XslFilename other)
+			{
+				if (other == null)
+					return false;
+
+				if (!string.Equals(other.Filename, Filename) || Debug != other.Debug)
+					return false;
+
+				return true;
+			}
+
+			#endregion
+		}
+	}
+}
