5b1f5aba7b509f72b5de4ad7cadf6391666b6434
Bert Huijben <rhuijben@users.noreply.github.com>
2013-11-03 18:53:44 +0000

    * src/Projects/TurtleBuild.Tests/PathTests.cs   Handle renamed and updated solution formats.

--- src/Projects/TurtleBuild.Tests/PathTests.cs
+++ src/Projects/TurtleBuild.Tests/PathTests.cs
@@ -8,57 +8,57 @@ using QQn.TurtleBuildUtils;
 
 namespace TurtleTests
 {
-	[TestFixture]
-	public class PathTests
-	{
-		[Test]
-		public void ExtensionEquals()
-		{
-			Assert.That(QQnPath.ExtensionEquals("c:/test.txt", ".txt"), Is.True);
-			Assert.That(QQnPath.ExtensionEquals("c:/test.txt", ".TXT"), Is.True);
-			Assert.That(QQnPath.ExtensionEquals("test.tXt", ".txt"), Is.True);
-		}
-
-		[Test]
-		public void EnsureRelativePath()
-		{
-			Assert.That(QQnPath.EnsureRelativePath("c:\\tools", "c:/tools/t.txt"), Is.EqualTo("t.txt"));
-			Assert.That(QQnPath.EnsureRelativePath("c:\\tools", "c:/t.txt"), Is.EqualTo("..\\t.txt"));
-
-			Assert.That(QQnPath.EnsureRelativePath("c:\\tools", "t.txt"), Is.EqualTo("t.txt"));
-			Assert.That(QQnPath.EnsureRelativePath("c:\\tools", "\\banaan\\t.txt"), Is.EqualTo("..\\banaan\\t.txt"));
-
-			string currentDisk = Path.GetPathRoot(Environment.CurrentDirectory);
-			string otherDisk;
-
-			if(string.Equals(currentDisk, "z:\\", StringComparison.OrdinalIgnoreCase))
-				otherDisk = "z:\\";
-			else
-				otherDisk = "c:\\";
-
-			Assert.That(Path.Combine(otherDisk + "tools", "\\t.txt"), Is.EqualTo("\\t.txt"), "Path.Combine works like .Net 2.0");
-			Assert.That(Path.GetFullPath(Path.Combine(otherDisk + "tools", "\\t.txt")), Is.EqualTo(currentDisk + "t.txt"), "Path.Combine works like .Net 2.0");
-			Assert.That(QQnPath.Combine(otherDisk + "tools", "\\t.txt"), Is.EqualTo( otherDisk + "t.txt"), "QQnPath combines always relative");
-
-
-			Assert.That(QQnPath.NormalizePath("c:\\", false), Is.EqualTo("c:\\"));
-		}
-
-		[Test]
-		public void GetFrameworkDirectory_11_ReturnsDirectory()
-		{
-			DirectoryInfo dir = BuildTools.GetFrameworkDirectory(new Version("1.1"));
-
-            Assert.That(dir, Is.Null);
-		}
+    [TestFixture]
+    public class PathTests
+    {
+        [Test]
+        public void ExtensionEquals()
+        {
+            Assert.That(QQnPath.ExtensionEquals("c:/test.txt", ".txt"), Is.True);
+            Assert.That(QQnPath.ExtensionEquals("c:/test.txt", ".TXT"), Is.True);
+            Assert.That(QQnPath.ExtensionEquals("test.tXt", ".txt"), Is.True);
+        }
 
         [Test]
-	    public void GetFrameworkDirectory_20_ReturnsDirectory()
-	    {
+        public void EnsureRelativePath()
+        {
+            Assert.That(QQnPath.EnsureRelativePath("c:\\tools", "c:/tools/t.txt"), Is.EqualTo("t.txt"));
+            Assert.That(QQnPath.EnsureRelativePath("c:\\tools", "c:/t.txt"), Is.EqualTo("..\\t.txt"));
+
+            Assert.That(QQnPath.EnsureRelativePath("c:\\tools", "t.txt"), Is.EqualTo("t.txt"));
+            Assert.That(QQnPath.EnsureRelativePath("c:\\tools", "\\banaan\\t.txt"), Is.EqualTo("..\\banaan\\t.txt"));
+
+            string currentDisk = Path.GetPathRoot(Environment.CurrentDirectory);
+            string otherDisk;
+
+            if (string.Equals(currentDisk, "z:\\", StringComparison.OrdinalIgnoreCase))
+                otherDisk = "z:\\";
+            else
+                otherDisk = "c:\\";
+
+            Assert.That(Path.Combine(otherDisk + "tools", "\\t.txt"), Is.EqualTo("\\t.txt"), "Path.Combine works like .Net 2.0");
+            Assert.That(Path.GetFullPath(Path.Combine(otherDisk + "tools", "\\t.txt")), Is.EqualTo(currentDisk + "t.txt"), "Path.Combine works like .Net 2.0");
+            Assert.That(QQnPath.Combine(otherDisk + "tools", "\\t.txt"), Is.EqualTo(otherDisk + "t.txt"), "QQnPath combines always relative");
+
+
+            Assert.That(QQnPath.NormalizePath("c:\\", false), Is.EqualTo("c:\\"));
+        }
+
+        [Test]
+        public void GetFrameworkDirectory_11_ReturnsDirectory()
+        {
+            DirectoryInfo dir = BuildTools.GetFrameworkDirectory(new Version("1.1"));
+
+            Assert.That(dir, Is.Null);
+        }
+
+        [Test]
+        public void GetFrameworkDirectory_20_ReturnsDirectory()
+        {
             DirectoryInfo dir = BuildTools.GetFrameworkDirectory(new Version("2.0"));
 
             Assert.That(dir.Name, Is.EqualTo("v2.0.50727"));
-	    }
+        }
 
         [Test]
         public void GetFrameworkDirectory_30_ReturnsDirectory()
@@ -92,41 +92,51 @@ namespace TurtleTests
             Assert.That(dir.Name, Is.EqualTo("v4.0.30319"));
         }
 
-	    [Test]
-		public void CheckSolutionVersions()
-		{
-			DirectoryInfo di = new DirectoryInfo(Environment.CurrentDirectory);
-			while (di.Parent.FullName != di.Root.FullName)
-			{
-				bool breakOut = false;
-				foreach (FileInfo f in di.GetFiles("*.sln", SearchOption.TopDirectoryOnly))
-				{
-					Version v = BuildTools.GetSolutionVersion(f.FullName);
+        [Test]
+        public void CheckSolutionVersions()
+        {
+            DirectoryInfo di = new DirectoryInfo(Environment.CurrentDirectory);
+            while (di.Parent.FullName != di.Root.FullName)
+            {
+                bool breakOut = false;
+                foreach (FileInfo f in di.GetFiles("*.sln", SearchOption.TopDirectoryOnly))
+                {
+                    Version v = BuildTools.GetSolutionVersion(f.FullName);
 
-					Assert.That(v, Is.Not.Null);
+                    Assert.That(v, Is.Not.Null);
 
-					Assert.That(v, Is.EqualTo(f.Name.Contains("2008") ? new Version("10.0") : new Version("9.0")));
-					breakOut = true;
-				}
-				if (breakOut)
-					break;
-				di = di.Parent;
-			}
-		}
+                    Version expectedVersion;
 
-		[Test]
-		public void VerifyTrueName()
-		{
-			foreach(Environment.SpecialFolder sv in Enum.GetValues(typeof(Environment.SpecialFolder)))
-			{
-				string path = Environment.GetFolderPath(sv);
+                    if (f.Name.Contains("2005"))
+                        expectedVersion = new Version(9, 0);
+                    else if (f.Name.Contains("2008"))
+                        expectedVersion = new Version(10, 0);
+                    else
+                        // VS 2012 / Portable format
+                        expectedVersion = new Version(12, 0);
 
-				if (string.IsNullOrEmpty(path))
-					continue;
+                    Assert.That(v, Is.EqualTo(expectedVersion));
+                    breakOut = true;
+                }
+                if (breakOut)
+                    break;
+                di = di.Parent;
+            }
+        }
 
-				Assert.That(QQnPath.GetTruePath(path.ToUpperInvariant()), Is.Not.Null);
-			}
-		}
+        [Test]
+        public void VerifyTrueName()
+        {
+            foreach (Environment.SpecialFolder sv in Enum.GetValues(typeof(Environment.SpecialFolder)))
+            {
+                string path = Environment.GetFolderPath(sv);
 
-	}
+                if (string.IsNullOrEmpty(path))
+                    continue;
+
+                Assert.That(QQnPath.GetTruePath(path.ToUpperInvariant()), Is.Not.Null);
+            }
+        }
+
+    }
 }
